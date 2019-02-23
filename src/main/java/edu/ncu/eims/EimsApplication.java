c09910851f543f9824f017d34a00665c2525bf19
hemenghai <289726753@qq.com>
2019-02-23 13:52:11 +0800

    init: 项目初始化
    上传基本项目

---
+++ src/main/java/edu/ncu/eims/EimsApplication.java
@@ -0,0 +1,13 @@
+package edu.ncu.eims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EimsApplication.class, args);
+	}
+
+}
