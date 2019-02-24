18fc3e8df41c75a1265361bf885d31afbf7da5c3
chenwq004 <1033500321@qq.com>
2019-02-24 17:22:45 +0800

    增加前端代码

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,62 @@
+$(function () {
+
+    layui.use('table', function(){
+        var table = layui.table;
+        table.render({
+            elem: '#enterpriseTable'
+            //,url:url
+            ,url: './enterprise'
+            ,cols: [[
+                {field:'enterpriseName', title: '企业名称'}
+                ,{field:'enterpriseNumber', title: '企业代码'}
+                ,{field:'enterpriseScale', title: '企业规模'}
+                ,{field:'legalPerson', title: '法人'}
+                ,{field:'establishmentTime', title: '成立时间'}
+                ,{field:'contactNumber', title: '联系号码'}
+                ,{field:'industryCategory', title: '行业类别'}
+                ,{field:'industryName', title: '行业名称'}
+                ,{field:'industryChain', title: '所属产业链'}
+                ,{field:'mainBusiness1', title: '主营业务1'}
+                ,{field:'mainBusiness2', title: '主营业务2'}
+            ]]
+            ,page: true
+            ,id:"testReload"
+            ,request: {
+                pageName: 'page' //页码的参数名称，默认：page
+                ,limitName: 'size' //每页数据量的参数名，默认：limit
+            }
+        });
+        var $ = layui.$, active = {
+            reload: function(){
+                var reloadEle = $('#enterpriseTable');
+                //执行重载
+                table.reload('testReload', {
+                    page: {
+                        curr: 1 //重新从第 1 页开始
+                    }
+                    ,where: {
+                        key: {
+                            id: reloadEle.val()
+                        }
+                    }
+                });
+            }
+        };
+        //数据查询
+        $("#query").click(function () {
+            var name = $("#name").val();
+            var scale = $ ("#scale").val();
+            var category = $("#category").val();
+            var chain = $("#chain").val();
+            var myTable = $("#enterpriseTable");
+            var type = $(this).data('type');
+            active[type] ? active[type].call(this) : '';
+        });
+        function renderTable(table,url) {
+
+        }
+    });
+
+
+
+});
