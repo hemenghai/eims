3c7538d60662fbaff2ed3cbd3ad242c253e372c4
hemenghai <289726753@qq.com>
2019-02-24 18:44:54 +0800

    update: 格式化日期格式
    将日期的json格式改为年月日，并处理时区

--- src/main/java/edu/ncu/eims/entity/Enterprise.java
+++ src/main/java/edu/ncu/eims/entity/Enterprise.java
@@ -1,9 +1,11 @@
 package edu.ncu.eims.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -61,6 +63,7 @@ public class Enterprise {
      * 成立时间
      */
     @Column(name = "establishment_time")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date establishmentTime;
     /**
      * 联系号码
